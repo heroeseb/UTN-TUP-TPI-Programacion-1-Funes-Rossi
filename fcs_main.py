30e30d66c7b9203dcfac6b60fbbe34d447b97d92
Pablo Sebastian Funes <109395899+heroeseb@users.noreply.github.com>
2026-05-25 12:18:59 -0300

    Uno ambas ramas

--- fcs_main.py
+++ fcs_main.py
@@ -1,8 +1,8 @@
 import csv
 import os
 import questionary
-
 csv_ruta = 'paises.csv'
+paises = []
 
 # Función para cargar datos
 def cargar_datos(ruta):
@@ -18,40 +18,25 @@ def cargar_datos(ruta):
                     fila['continente'] = fila['continente']
                     paises.append(fila)
                 except (ValueError, KeyError, TypeError):
-                    print('Se ignoró una línea inválida del CSV')
+                    print('Se ignoró una línea inválida del CSV.')
     except FileNotFoundError:
         print('El archivo no existe. Se creará una lista vacía.')
     except PermissionError:
         print('Error... No tenés permiso de lectura en este archivo.')
     return paises
 
-# Seleccionar con questionary
-def seleccionar_menu():
-    opcion = questionary.select(
-        message="Seleccioná:",
-        choices=['1. Agregar un país.','2. Actualizar los datos de Población y Superficie de un País.',
-                '3. Buscar un país por nombre.','4. Filtrar países.',
-                '5. Ordenar países.','6. Mostrar estadísticas.','7. Salir.']
-    ).ask()
-    return opcion
+# Función para guardar datos
+Fieldnames = ['nombre', 'poblacion', 'superficie', 'continente']
 
-# Limpiar consola
-def limpiar_consola():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-# Función para continuar
-def continuar():
-    input("Presione cualquier tecla para continuar")
-
-# Función de menu
-def menu():
-    print('1. Agregar un país.')
-    print('2. Actualizar los datos de Población y Superficie de un País.')
-    print('3. Buscar un país por nombre.')
-    print('4. Filtrar países.')
-    print('5. Ordenar países.')
-    print('6. Mostrar estadísticas.')
-    print('7. Salir.')
+def guardar_datos(lista):
+    try:
+        with open(csv_ruta, 'w', newline='', encoding='utf-8') as archivo:
+            writer = csv.DictWriter(archivo, fieldnames=Fieldnames)
+            writer.writeheader()
+            writer.writerows(lista)
+            print('Se guardaron correctamente los datos.')
+    except PermissionError:
+        print('¡Error! No se pudo guardar. El archivo está abierto por otro programa o no hay permisos.')
 
 # Validación de inputs
 def input_str(mensaje, mensaje_2=None):
@@ -71,6 +56,41 @@ def input_int(mensaje, mensaje_2=None):
             return input_salid
         except ValueError:
             print(mensaje_2 if mensaje_2 else 'Error de valor')
+
+# Limpiar consola
+def limpiar_consola():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Función para continuar
+def continuar():
+    input("Presione cualquier tecla para continuar")
+
+# funcion para quitar tildes
+def quitar_tildes(texto):
+    texto = texto.lower()
+    reemplazos = {
+        'á': 'a',
+        'é': 'e',
+        'í': 'i',
+        'ó': 'o',
+        'ú': 'u',
+        'ü': 'u',
+        'ñ': 'n'
+    }
+    for letra_tilde, letra_normal in reemplazos.items():
+        texto = texto.replace(letra_tilde, letra_normal)
+    return texto
+
+
+# Seleccionar con questionary
+def seleccionar_menu():
+    opcion = questionary.select(
+        message="Seleccione la acción a realizar:",
+        choices=['1. Agregar un país.','2. Actualizar los datos de Población y Superficie de un País.',
+                '3. Buscar un país por nombre.','4. Filtrar países.',
+                '5. Ordenar países.','6. Mostrar estadísticas.','7. Salir.']
+    ).ask()
+    return opcion
 
 # Punto 1
 def agregar_pais(lista):
@@ -98,35 +118,27 @@ def agregar_pais(lista):
         print('¡Se agregó correctamente el país!')
     return lista
 
-# Función para guardar datos
-Fieldnames = ['nombre', 'poblacion', 'superficie', 'continente']
-
-def guardar_datos(lista):
-    try:
-        with open(csv_ruta, 'w', newline='', encoding='utf-8') as archivo:
-            writer = csv.DictWriter(archivo, fieldnames=Fieldnames)
-            writer.writeheader()
-            writer.writerows(lista)
-            print('Se guardaron correctamente los datos.')
-    except PermissionError:
-        print('¡Error! No se pudo guardar. El archivo está abierto por otro programa o no hay permisos.')
+# punto 2
+def actualizar_datos_pys(paises):
+    encontrado = False
+    pais_actu = input_str("¿Cual país desea actualizar?: ","Inserte un nombre que se encuentre en la lista.").capitalize()
+    for d in paises:
+        if pais_actu == d["nombre"]:
+            encontrado = True  
+            nv_poblacion = input_int(f"¿Cual sera la nueva poblacion para {pais_actu}?: ","Inserte un valor valido.")
+            while not nv_poblacion > 0:
+                print("La cantidad de poblacion debe ser un valor numerico mayor a cero.")
+                nv_poblacion = input_int(f"¿Cual sera la nueva poblacion para {pais_actu}?: ","Inserte un valor valido.")
+            nv_superficie = input_int(f"¿Cual sera la nueva superficie para {pais_actu}?: ", "Inserte un valor valido.")
+            while not nv_superficie > 0:
+                print("La cantidad de superficie debe ser un valor numerico mayor a cero.")
+                nv_superficie = input_int(f"¿Cual sera la nueva superficie para {pais_actu}?: ", "Inserte un valor valido.")
+            d["poblacion"] = nv_poblacion 
+            d["superficie"] = nv_superficie
+            print("País actualizado correctamente.")
+    if not encontrado: print("No se encontro un país...")
 
 # Punto 3
-def quitar_tildes(texto):
-    texto = texto.lower()
-    reemplazos = {
-        'á': 'a',
-        'é': 'e',
-        'í': 'i',
-        'ó': 'o',
-        'ú': 'u',
-        'ü': 'u',
-        'ñ': 'n'
-    }
-    for letra_tilde, letra_normal in reemplazos.items():
-        texto = texto.replace(letra_tilde, letra_normal)
-    return texto
-
 def buscar_pais(lista):
     '''Buscar un país por nombre (coincidencia parcial o exacta).'''
     encontro_coincidencia = False
@@ -139,6 +151,63 @@ def buscar_pais(lista):
                 print(f"Nombre del país: {lista[i]['nombre']} | Población: {lista[i]['poblacion']} | Superficie: {lista[i]['superficie']} | Continente: {lista[i]['continente']}")
     if not encontro_coincidencia:
         print('No se encontraron coincidencias.')
+
+# punto 4
+def filtrado_paises(paises):
+    while True:
+        print("""Eliga el filtro:
+1) Continente;
+2) Rango de población;
+3) Rango de superficie;
+4) Volver atras.""")
+        opcion = input_int("","Ingrese un valor numerico valido")
+        match opcion:
+            case 1:
+                encontrado = False
+                while True:
+                    filtro = input_str("Ingrese el continente: ","Trate de ingresar el nombre correctamente.").capitalize()
+                    for d in paises:
+                        if filtro == d["continente"]: encontrado = True , print(f"-{d["nombre"]}.")
+                        elif filtro == "America":
+                            filtro = "América"
+                            if filtro == d["continente"]: encontrado = True , print(f"-{d["nombre"]}.")
+                        elif filtro == "Africa":
+                            filtro = "África"
+                            if filtro == d["continente"]: encontrado = True , print(f"-{d["nombre"]}.")
+                        elif filtro == "Oceania":
+                            filtro = "Oceanía"
+                            if filtro == d["continente"]: encontrado = True , print(f"-{d["nombre"]}.") 
+                    if not encontrado: print("No hay ningun país con ese continente.")
+                    break
+            case 2:
+                encontrado = False
+                while True:
+                    filtro = input_int("Ingrese el comienzo del rango: ","Trate de ingresar un valor numerico valido.")
+                    while not filtro > 0:
+                        print("La cantidad debe ser mayor a cero. Minimo 1.")
+                        filtro = input_int("Ingrese el comienzo del rango: ","Trate de ingresar un valor numerico valido.")
+                    filtro2 = input_int("Ingrese el final del rango: ","Trate de ingresar un valor numerico valido.")
+                    for d in paises:
+                        if filtro <= d["poblacion"] and d["poblacion"] <= filtro2: encontrado = True , print(f"-{d["nombre"]}.")
+                    if not encontrado: print("No hay ningun país que coincida con ese rango de población.")
+                    break
+            case 3:
+                encontrado = False
+                while True:
+                    filtro = input_int("Ingrese el comienzo del rango: ","Trate de ingresar un valor numerico valido.")
+                    while not filtro > 0:
+                        print("La cantidad debe ser mayor a cero. Minimo 1.")
+                        filtro = input_int("Ingrese el comienzo del rango: ","Trate de ingresar un valor numerico valido.")
+                    filtro2 = input_int("Ingrese el final del rango: ","Trate de ingresar un valor numerico valido.")
+                    for d in paises:
+                        if filtro <= d["superficie"] and d["superficie"] <= filtro2: encontrado = True , print(f"-{d["nombre"]}.")
+                    if not encontrado: print("No hay ningun país que coincida con ese rango de superficie.")
+                    break
+            case 4:
+                print("Volviendo...")
+                break
+            case _:
+                print("Eliga una opción presentada en pantalla.")
 
 # Punto 5
 def ordenar_paises(lista):
@@ -168,18 +237,62 @@ def ordenar_paises(lista):
             print('¡Por favor ingrese una opción correcta!')
     return lista
 
+# punto 6
+def mostrar_estadisticas(paises):
+    lista_poblaciones = list(map(lambda x: x["poblacion"], paises))
+    lista_superficie = list(map(lambda x: x["superficie"], paises))
+    mayor = max(lista_poblaciones)
+    menor = min(lista_poblaciones)
+    for d in paises:
+        if mayor == d["poblacion"]: pais_ma = d["nombre"]
+        if menor == d["poblacion"]: pais_me = d["nombre"]
+    print(f"""El país con mayor población es: {pais_ma}, con una cantidad de: {mayor} habitantes.
+El país con mayor población es: {pais_me}, con una cantidad de: {menor} habitantes.""")
+    print(f"El promedio de población es: {sum(lista_poblaciones) / len(lista_poblaciones)}.")
+    print(f"El promedio de superficie es: {sum(lista_superficie) / len(lista_superficie)}.")
+    americanos = 0
+    europeos = 0
+    africanos = 0
+    asiaticos = 0
+    oceanicos = 0
+    for d in paises:
+        if d["continente"] == "América": americanos += 1
+        elif d["continente"] == "África": africanos += 1
+        elif d["continente"] == "Europa": europeos += 1
+        elif d["continente"] == "Asia": asiaticos += 1
+        elif d["continente"] == "Oceanía": oceanicos += 1
+    print(f"""Cantidad de paises en..:
+América: {americanos};
+Europa: {europeos};
+Asia: {asiaticos};
+África: {africanos};
+Oceanía: {oceanicos}.""")
+
+
 if __name__ == '__main__':
-    paises = []
     print('Iniciamos lista y cargamos datos')
     paises = cargar_datos(csv_ruta)
     print(paises)
+    
     print('Punto 1')
     paises = agregar_pais(paises)
-    print('Guardo países en el csv')
-    guardar_datos(paises)
-    print(paises)
+    
+    print('punto 2')
+    actualizar_datos_pys(paises)
+    
     print('Punto 3, buscar países')
     buscar_pais(paises)
+    
+    print('punto 4')
+    filtrado_paises(paises)
+    
     print('Punto 5,ordenar países')
     ordenar_paises(paises)
+    print(paises)
+    
+    print("punto 6")
+    mostrar_estadisticas(paises)
+    
+    print('Guardo países en el csv')
+    guardar_datos(paises)
     print(paises)
--- main.py
+++ main.py
@@ -3,27 +3,27 @@ from fcs_main import *
 paises = cargar_datos(csv_ruta)
 
 while True:
+    limpiar_consola()
     opcion = seleccionar_menu()
     match opcion:
         case '1. Agregar un país.':
             paises = agregar_pais(paises)
             guardar_datos()
             continuar()
-            limpiar_consola()
         case '2. Actualizar los datos de Población y Superficie de un País.':
-            pass
+            actualizar_datos_pys(paises)
+            continuar()
         case '3. Buscar un país por nombre.':
             buscar_pais(paises)
             continuar()
-            limpiar_consola()
         case '4. Filtrar países.':
-            pass
+            filtrado_paises(paises) # arreglar sub-menu
         case '5. Ordenar países.':
             ordenar_paises(paises)
             continuar()
-            limpiar_consola()
         case '6. Mostrar estadísticas.':
-            pass
+            mostrar_estadisticas(paises)
+            continuar()
         case '7. Salir.':
-            print('¡Saliendo del programa!')
+            print('Hasta luego, ¡vuelva pronto!')
             break
